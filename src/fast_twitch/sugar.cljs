0d489237a536879c86d5f8251a9a1f6d031e3dd5
outcastgeek <outcastgeek+git@gmail.com>
2020-03-08 03:22:39 -0400

    Added Support for Cookies

--- src/fast_twitch/sugar.cljs
+++ src/fast_twitch/sugar.cljs
@@ -4,6 +4,7 @@
   (:require [cljs.nodejs :as nodejs]
             [clojure.string :refer [lower-case]]
             [clojure.walk :refer [keywordize-keys]]
+            [taoensso.timbre :as log]
             ["express" :as express]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -11,90 +12,105 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn app
-  "Create a express app instance."
-  []
-  (express))
+      "Create a express app instance."
+      []
+      (express))
 
 (defn router
-  "Create a express router instance."
-  [& [opts]]
-  (.Router express (clj->js opts)))
+      "Create a express router instance."
+      [& [opts]]
+      (.Router express (clj->js opts)))
 
 (defn routes
-  "Create a router instance and attach
-  handlers to it."
-  [& args]
-  (let [app (router)]
-    (reduce (fn [app [verb path handler]]
-              (condp = verb
-                :all (. app (all path handler))
-                :get (. app (get path handler))
-                :post (. app (post path handler))
-                :put (. app (put path handler))
-                :delete (. app (delete path handler))
-                app))
-            app
-            args)))
+      "Create a router instance and attach
+      handlers to it."
+      [& args]
+      (let [app (router)]
+           (reduce (fn [app [verb path handler]]
+                       (condp = verb
+                              :all (. app (all path handler))
+                              :get (. app (get path handler))
+                              :post (. app (post path handler))
+                              :put (. app (put path handler))
+                              :delete (. app (delete path handler))
+                              app))
+                   app
+                   args)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Request & Response Helpers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn send
-  [res data]
-  (.send res data))
+      [res data]
+      (.send res data))
 
 (defn status
-  [res code]
-  (.status res code))
+      [res code]
+      (.status res code))
 
 (defn set-headers
-  [res headers]
-  (.set res headers))
+      [res headers]
+      (.set res (clj->js headers)))
 
 (defn get-headers
-  [req]
-  (let [headers (-> (.-headers req) (js->clj :keywordize-keys true))]
-    ;;(log/debug "Request Headers: " headers)
-    headers))
+      [req]
+      (let [headers (-> (.-headers req) (js->clj :keywordize-keys true))]
+           ;;(log/debug "Request Headers: " headers)
+           headers))
 
 (defn path
-  [req]
-  (let [path (.-url req)]
-    path))
+      [req]
+      (let [path (.-url req)]
+           path))
 
 (defn full-url
-  [req]
-  (let [protocol (.-protocol req)
-        host (.get req "host")
-        ;;port (.. req -app -settings -port)
-        raw-path (.-url req)
-        path (path req)]
-    ;;(log/debug "URL Parts: " protocol " + " host " + " port " + " url)
-    (str
-     protocol "://" host path)))
+      [req]
+      (let [protocol (.-protocol req)
+            host (.get req "host")
+            ;;port (.. req -app -settings -port)
+            raw-path (.-url req)
+            path (path req)]
+           ;;(log/debug "URL Parts: " protocol " + " host " + " port " + " url)
+           (str
+             protocol "://" host path)))
 
 (defn method
-  [req]
-  (let [m (.-method req)]
-    (-> m
-        lower-case
-        keyword)))
+      [req]
+      (let [m (.-method req)]
+           (-> m
+               lower-case
+               keyword)))
 
 (defn body
-  [req]
-  (-> req
-      .-body
-      (js->clj :keywordize-keys true)))
+      [req]
+      (-> req
+          .-body
+          (js->clj :keywordize-keys true)))
 
 (defn csrf-token
-  [req]
-  (.csrfToken req))
+      [req]
+      (.csrfToken req))
+
+(defn set-cookies
+      [res cookies]
+      (when (not-empty cookies)
+            (log/debug "Setting Cookies: " cookies))
+      (doseq [c cookies
+              :let [{:keys [name value opts]} c]]
+             (.cookie res name value (clj->js opts)))
+      ;; Make sure to return the response object!!!!
+      res)
 
 (defn cookies [req]
-  (-> req
-      .-cookies
-      (js->clj :keywordize-keys true)))
+      (-> req
+          .-cookies
+          (js->clj :keywordize-keys true)))
+
+(defn signed-cookies [req]
+      (-> req
+          .-signedCookies
+          (js->clj :keywordize-keys true)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -102,18 +118,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn with-middleware
-  "Attach middlware to express app."
-  ([app handler]
-   (.use app handler))
-  ([app path handler]
-   (.use app path handler)))
+      "Attach middlware to express app."
+      ([app handler]
+       (.use app handler))
+      ([app path handler]
+       (.use app path handler)))
 
 (defn listen
-  "Shortcut for start http server."
-  ([app port]
-   (.listen app port "0.0.0.0"))
-  ([app port callback]
-   (.listen app port "0.0.0.0" callback)))
+      "Shortcut for start http server."
+      ([app port]
+       (.listen app port "0.0.0.0"))
+      ([app port callback]
+       (.listen app port "0.0.0.0" callback)))
 
 ;; (defn set
 ;;   [app key value]
@@ -125,22 +141,22 @@
 ;;   (.get app key))
 
 (defn enable
-  [app key]
-  (.enable app key)
-  app)
+      [app key]
+      (.enable app key)
+      app)
 
 (defn disable
-  [app key]
-  (.disable app key)
-  app)
+      [app key]
+      (.disable app key)
+      app)
 
 (defn enabled?
-  [app key]
-  (.enabled app key))
+      [app key]
+      (.enabled app key))
 
 (defn disabled?
-  [app key]
-  (.disabled app key))
+      [app key]
+      (.disabled app key))
 
 ;(defn static
 ;  ([app path]
--- src/fast_twitch/web_api.cljs
+++ src/fast_twitch/web_api.cljs
@@ -16,37 +16,40 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- render-to-str [widget]
-  (rs/render-to-string widget))
+       (rs/render-to-string widget))
 
 (defn- render
-  ([body] (pr-str body))
-  ([format body]
-   (log/debug "Response Format: " format)
-   (condp = format
-     :html (str
-            "<!DOCTYPE html>"
-            (render-to-str body))
-     :json (let [w (transit/writer :json)]
-             (transit/write w body))
-     (pr-str body))))
+       ([body] (pr-str body))
+       ([format body]
+        (log/debug "Response Format: " format)
+        (condp = format
+               :html (str
+                       "<!DOCTYPE html>"
+                       (render-to-str body))
+               :json (let [w (transit/writer :json)]
+                          (transit/write w body))
+               (pr-str body))))
 
 (defn- build-response
-  ([body]
-   (identity
-    {:status 200
-     :headers {}
-     :body (render body)}))
-  ([fmt body]
-   (identity
-    {:status 200
-     :headers {}
-     :body (render fmt body)}))
-  ([fmt body options]
-   (log/debug "Options: " options)
-   (let [{:keys [headers status] :or {headers {}, status 200}} options]
-     {:status status
-      :headers (clj->js headers)
-      :body (render fmt body)})))
+       ([body]
+        (identity
+          {:status  200
+           :headers {}
+           :cookies []
+           :body    (render body)}))
+       ([fmt body]
+        (identity
+          {:status  200
+           :headers {}
+           :cookies []
+           :body    (render fmt body)}))
+       ([fmt body options]
+        (log/debug "Options: " options)
+        (let [{:keys [cookies headers status] :or {cookies [], headers {}, status 200}} options]
+             {:status  status
+              :headers (clj->js headers)
+              :cookies cookies
+              :body    (render fmt body)})))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -58,85 +61,92 @@
 ;   (satisfies? clojure.core.async.impl.protocols/Channel x))
 
 (defn- channel?
-  [x] ;; Clojurescript
-  (satisfies? cljs.core.async.impl.protocols/Channel x))
+       [x]                                                  ;; Clojurescript
+       (satisfies? cljs.core.async.impl.protocols/Channel x))
 
 (defn- respond
-  [res data]
-  (let [{status :status headers :headers body :body} data]
-        ;;(log/debug "Response Data: " (-> data pprint with-out-str))
-    (-> res
-        (ft/status status)
-        (ft/set-headers (clj->js headers))
-        (ft/send body))))
+       [res data]
+       (let [{status :status headers :headers cookies :cookies body :body} data]
+            ;;(log/debug "Response Data: " (-> data pprint with-out-str))
+            (-> res
+                (ft/status status)
+                (ft/set-headers headers)
+                (ft/set-cookies cookies)
+                (ft/send body))))
 
 (defn match-route [rts path method]
-  (log/debug "Matching: " path " and " method)
-  (let [match-data (or
-                    (bidi/match-route*
-                     rts
-                     path
-                     {:request-method method})
-                    {:route-params {}
-                     :query-params {}
-                     :request-method method
-                     :handler (fn [req]
-                                (send "Not Found"))})]
-    (log/debug "Matched Route Data: " (-> match-data pprint with-out-str))
-    match-data))
+      (log/debug "Matching: " path " and " method)
+      (let [match-data (or
+                         (bidi/match-route*
+                           rts
+                           path
+                           {:request-method method})
+                         {:route-params   {}
+                          :query-params   {}
+                          :request-method method
+                          :handler        (fn [req]
+                                              (send "Not Found"))})]
+           (log/debug "Matched Route Data: " (-> match-data pprint with-out-str))
+           match-data))
 
 (defn- route-dispatcher [rts handler-fn req res]
-  (let [headers (ft/get-headers req)
-        path (ft/path req)
-        full-url (ft/full-url req)
-        method (ft/method req)
-        body (ft/body req)
-        cookies (ft/cookies req)
-        csrf-token (ft/csrf-token req)
-        match-data (match-route rts path method)
-        {route-params :route-params
-         query-params :query-params
-         request-method :request-method
-         key-handler :handler} match-data]
-    (let [req-data {:headers headers
-                    :route-params route-params
-                    :query-params query-params
-                    :body body
-                    :csrf-token csrf-token
-                    :cookies cookies
-                    :path path
-                    :full-url full-url
-                    :request-method request-method}]
-      (log/debug "Request Data: " (-> req-data pprint with-out-str))
-      (let [full-req (merge req-data {:raw-req req})
-            data (handler-fn
-                  {:endpoint key-handler
-                   :req full-req})]
-        (if (channel? data)
-          (go
-            (let [response-data (<! data)]
-                ;;(log/debug "Response Data: " (-> response-data pprint with-out-str))
-              (respond res response-data)))
-          (do
-            ;;(log/debug "Response Data: " (-> data pprint with-out-str))
-            (respond res data))))
-      )))
+       (let [headers (ft/get-headers req)
+             path (ft/path req)
+             full-url (ft/full-url req)
+             method (ft/method req)
+             body (ft/body req)
+             cookies (ft/cookies req)
+             signed-cookies (ft/signed-cookies req)
+             csrf-token (ft/csrf-token req)
+             match-data (match-route rts path method)
+             {route-params   :route-params
+              query-params   :query-params
+              request-method :request-method
+              key-handler    :handler} match-data]
+            (let [req-data {:headers        headers
+                            :route-params   route-params
+                            :query-params   query-params
+                            :body           body
+                            :csrf-token     csrf-token
+                            :cookies        cookies
+                            :signed-cookies signed-cookies
+                            :path           path
+                            :full-url       full-url
+                            :request-method request-method}]
+                 (log/debug "Request Data: " (-> req-data pprint with-out-str))
+                 (let [full-req (merge req-data {:raw-req req})
+                       data (handler-fn
+                              {:endpoint key-handler
+                               :req      full-req})]
+                      (if (channel? data)
+                        (go
+                          (let [response-data (<! data)]
+                               ;;(log/debug "Response Data: " (-> response-data pprint with-out-str))
+                               (respond res response-data)))
+                        (do
+                          ;;(log/debug "Response Data: " (-> data pprint with-out-str))
+                          (respond res data))))
+                 )))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; API
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn routes [rts handler-fn]
-  (reset! cached-routes rts)
-  (ft/routes [:all "*" (fn [req res]
-                         ;;(log/debug "Route Definitions: " (-> rts pprint with-out-str))
-                         (route-dispatcher rts handler-fn req res))]))
+      (reset! cached-routes rts)
+      (ft/routes [:all "*" (fn [req res]
+                               ;;(log/debug "Route Definitions: " (-> rts pprint with-out-str))
+                               (route-dispatcher rts handler-fn req res))]))
 
 (defn send
-  [& data]
-  (apply build-response data))
+      [& data]
+      (apply build-response data))
 
-(defn redirect [route-key]
-  (send :proceed (name route-key)
-        {:headers {:location (bidi/path-for @cached-routes route-key)}
-         :status  301}))
+(defn redirect [route-key & {:keys [cookies status] :or {status 301}}]
+      (do
+        (when cookies
+              (log/debug "Redirect Cookies: " cookies))
+        (send :proceed (name route-key)
+              {:headers {:location (bidi/path-for @cached-routes route-key)}
+               :cookies cookies
+               :status  status})))
