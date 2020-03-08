cffce60daa7f9e73a2290259e4ecbe5dfafcedde
outcastgeek <outcastgeek+git@gmail.com>
2020-03-07 22:36:07 -0500

    Updated Styles + Scripts preload.cljs with more functionality

--- src/fast_twitch/preload.cljs
+++ src/fast_twitch/preload.cljs
@@ -1,4 +1,5 @@
-(ns fast-twitch.preload)
+(ns fast-twitch.preload
+  (:require [cljs.core.match :refer-macros [match]]))
 
 ;; Reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content
 
@@ -28,3 +29,32 @@
 
 (defn quick-work [path]
   [:link {:rel "preload" :href path :as "worker"}])
+
+(defn preload-styles [styles]
+      (for [style-data styles]
+           ^{:key style-data}
+           (match style-data
+                  [:href href] [quick-css href]
+                  :else identity)))
+
+(defn preload-scripts [scripts]
+      (for [script-data scripts]
+           ^{:key script-data}
+           (match script-data
+                  [:src src] [quick-js src]
+                  :else identity)))
+
+(defn load-styles [styles]
+      (for [style-data styles]
+           ^{:key style-data}
+           (match style-data
+                  [:href href] [:link {:rel "stylesheet" :href href}]
+                  :else identity)))
+
+(defn load-scripts [scripts]
+      (for [script-data scripts]
+           ^{:key script-data}
+           (match script-data
+                  [:src src] [:script {:type "text/javascript" :src src}]
+                  [:src-txt src-txt] [:script {:type "text/javascript"} src-txt]
+                  :else identity)))
